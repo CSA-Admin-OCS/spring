40ef9d4c00c08edbbca6a6870ade0940ff2d563e
Jacob Chou <choujacob31@gmail.com>
2026-09-14 02:59:00 -0700

    phase 2 jpa implementation

--- src/main/java/com/open/spring/mvc/questboard/Questboard.java
+++ src/main/java/com/open/spring/mvc/questboard/Questboard.java
@@ -1,5 +1,9 @@
 package com.open.spring.mvc.questboard;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +11,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Entity
 public class Questboard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
     private String title;
     private String description;
